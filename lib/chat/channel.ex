45774d7f78a7f47903319647931d16d261dda112
Abdullah AlDwyish <aldwyish@student.unimelb.edu.au>
2014-11-26 14:17:15 +1100

    intial channel support

--- lib/chat/channel.ex
+++ lib/chat/channel.ex
@@ -1,15 +1,17 @@
 defmodule ChatChannel do
-  # use Phoenix.Channel, for "rooms", "lobby"
-  #
-  # event "new:user", do:
-  #
+  use Phoenix.Channel
+
+  #join "ws://localhost:4000/ws"
+
+
+  # event "new:user" do
+  #  Chat.add_user
   # end
   #
   # event "new:msg", do:
-  #
+  # Chat.add_msg
   # end
-  #
-  #
+
   # def event(name,callback) do
   #
   # end
--- lib/phoenix/channel.ex
+++ lib/phoenix/channel.ex
@@ -1,7 +1,16 @@
 defmodule Phoenix.Channel do
+  alias Phoenix.Socket
 
-  # defmacro __using__  do
-  #
-  # end
+  defmacro __using__(options)  do
+    quote do
+      import Phoenix.Channel
+      # import Phoenix.Socket [only: send]
+    end
+  end
 
+  def connect(url, channel, topic) do
+    IO.puts "connecting... #{endpoint}"
+    {:ok, socket} = Socket.start_link(pid,endpoint)
+    %{socket: socket, channel: channel, topic: topic}
+  end
 end
